4fce2966f77830e553d452859ad0686291924c12
Racheal <rachealynn@outlook.com>
2020-09-13 19:39:00 -0500

    change

--- src/App.js
+++ src/App.js
@@ -14,9 +14,9 @@ function App() {
   return (
     <div className="container">
       <nav class="navbar navbar-expand-sm navbar-light bg-dark">
-{/* <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 <span class="navbar-toggler-icon"></span>
-</button> */}
+</button> 
 <div class="navbar-collapse" id="navbarNav">
 <ul class="navbar-nav">
     <li class="nav-item active">
