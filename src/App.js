b3d268014ef2ec8f32c680601a8ba9792136838e
Racheal <rachealynn@outlook.com>
2020-09-13 19:20:38 -0500

    change

--- src/App.js
+++ src/App.js
@@ -13,12 +13,41 @@ import gith from './components/gith.png';
 function App() {
   return (
     <div className="container">
-
+      <nav class="navbar navbar-expand-sm navbar-light bg-dark">
+{/* <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+<span class="navbar-toggler-icon"></span>
+</button> */}
+<div class="navbar-collapse" id="navbarNav">
+<ul class="navbar-nav">
+    <li class="nav-item active">
+    <Link class="link nav-link text-light" to="/">Home <span class="sr-only">(current)</span></Link>
+    </li>
+    <li class="nav-item">
+    <Link class=" link nav-link text-light" to="/projects">Projects</Link>
+    </li>
+    <li class="nav-item">
+    <Link class="link nav-link text-light" to="/aboutme">More about me</Link>
+    </li>
+    <li class="nav-item">
+    <a class="link nav-link text-light" href="mailto:rachealynn@outlook.com">Contact me</a>
+    </li>
+    
+</ul>
+</div>
+</nav>
       <Router>
         <Welcome path="/" />
         <About path="/aboutme" />
         <Project path="/projects" />
       </Router>
+      <div className="footer text-light">
+      <h2 className="foot">Looking for a full stack web developer? <a href="mailto:rachealynn@outlook.com" className="main-btn email-lnk">Get in touch!</a></h2>
+      <div className="bottom">
+        <a href="https://github.com/Racheal-Mayer" className="main-btn email-lnk"><img src={gith} alt="" className="icon1"/> </a>
+        <a href="https://www.linkedin.com/in/racheal-mayer/" className="main-btn email-lnk"><img src={lin} alt="" className="icon"/> </a>
+    <a href="https://www.dropbox.com/s/6mt1jy8aptp5ofy/Racheal%20Mayer.docx?dl=0"><img src={resume} alt="" className="icon"/></a>
+    </div>
+    </div>
 </div>
   );
 }
