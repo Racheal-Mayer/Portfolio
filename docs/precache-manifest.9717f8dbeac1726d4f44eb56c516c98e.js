916efddbc6f8ff4a5f49c7bef5a83dc401cc6ffd
Racheal <rachealynn@outlook.com>
2020-09-13 22:11:04 -0500

    initial

---
+++ docs/precache-manifest.9717f8dbeac1726d4f44eb56c516c98e.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "043cb6a987b2b8dc549f18de02f1afab",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "90d70eee68edfe6b01dc",
+    "url": "/Portfolio/static/css/2.fb859a20.chunk.css"
+  },
+  {
+    "revision": "4b6c26b39e0aa4ad2501",
+    "url": "/Portfolio/static/css/main.8ee7da30.chunk.css"
+  },
+  {
+    "revision": "90d70eee68edfe6b01dc",
+    "url": "/Portfolio/static/js/2.e534e8d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.e534e8d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b6c26b39e0aa4ad2501",
+    "url": "/Portfolio/static/js/main.a5f9a9ce.chunk.js"
+  },
+  {
+    "revision": "efed155d89bc85b81984",
+    "url": "/Portfolio/static/js/runtime-main.88fabfc4.js"
+  },
+  {
+    "revision": "f94f07672bde2ca1d5f926d52f0417bf",
+    "url": "/Portfolio/static/media/CC.f94f0767.png"
+  },
+  {
+    "revision": "d9a57661c18384371a741ba1311d1bdc",
+    "url": "/Portfolio/static/media/CC2.d9a57661.png"
+  },
+  {
+    "revision": "d5bf3d79c0807e7b3d71ed86e89446dd",
+    "url": "/Portfolio/static/media/CC3.d5bf3d79.png"
+  },
+  {
+    "revision": "80fad765efeb0d273370087f9a68814c",
+    "url": "/Portfolio/static/media/CC4.80fad765.png"
+  },
+  {
+    "revision": "aa360d9cbc220dcdb2f23f8dc4a10624",
+    "url": "/Portfolio/static/media/CC5.aa360d9c.png"
+  },
+  {
+    "revision": "3bef657de1906c70ab10787571f64153",
+    "url": "/Portfolio/static/media/CC6.3bef657d.png"
+  },
+  {
+    "revision": "ef97eeaa4706a4d5626a58b68fd4c949",
+    "url": "/Portfolio/static/media/GREEN.ef97eeaa.png"
+  },
+  {
+    "revision": "75c25a95ead065fd0cc597a6ab2abe30",
+    "url": "/Portfolio/static/media/GREEN3.75c25a95.png"
+  },
+  {
+    "revision": "2409eb93e4b403b80728fdb29c9f4cfe",
+    "url": "/Portfolio/static/media/GREEN4.2409eb93.png"
+  },
+  {
+    "revision": "90128a01be4298ab05233c28d1e2ce2a",
+    "url": "/Portfolio/static/media/GREEN5.90128a01.png"
+  },
+  {
+    "revision": "132ce8a0a99ec1da4d6988d88bd87bcf",
+    "url": "/Portfolio/static/media/SOUTHPARK.132ce8a0.png"
+  },
+  {
+    "revision": "95d783820e3cc8d3702ad3f0812e27ee",
+    "url": "/Portfolio/static/media/SP2.95d78382.png"
+  },
+  {
+    "revision": "8be68a676cc0c2c830ff0f926d39ceaf",
+    "url": "/Portfolio/static/media/SP3.8be68a67.png"
+  },
+  {
+    "revision": "6afdec27de241e42e55d6fd1d691978d",
+    "url": "/Portfolio/static/media/cats.6afdec27.jpg"
+  },
+  {
+    "revision": "2be5173b426e15f3f2a6c5610ec142a7",
+    "url": "/Portfolio/static/media/cert.2be5173b.png"
+  },
+  {
+    "revision": "12048fae2b849c0b65216255f11fc4d5",
+    "url": "/Portfolio/static/media/gith.12048fae.png"
+  },
+  {
+    "revision": "ee834777a92667f7fa1a6c9b078c50dc",
+    "url": "/Portfolio/static/media/green2.ee834777.png"
+  },
+  {
+    "revision": "83c2d6ebbe5c2c07ddaa699c23d14f1e",
+    "url": "/Portfolio/static/media/pic.83c2d6eb.jpg"
+  }
+]);
